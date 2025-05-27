2e933e3440d6a0067622fce4d4023329f06b5e99
naeunkim <83413364+naeunkim1227@users.noreply.github.com>
2025-05-27 12:46:44 +0900

    Title: 007, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250527/007/007.py
@@ -0,0 +1,10 @@
+secret_code, meeting_point, time = input().split()
+time = int(time)
+
+# Please write your code here.
+result = (secret_code, meeting_point , time)
+code , point, t = result
+
+print("secret code :",code)
+print("meeting point :",point)
+print("time :",t)
