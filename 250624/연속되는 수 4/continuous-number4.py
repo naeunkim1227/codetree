775b8a280efe83b9d293510475ae55a99006ed16
naeunkim <83413364+naeunkim1227@users.noreply.github.com>
2025-06-24 12:36:49 +0900

    Title: 연속되는 수 4, Time: 54ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250624/연속되는 수 4/continuous-number4.py
@@ -0,0 +1,18 @@
+n = int(input())
+arr = [int(input()) for _ in range(n)]
+
+# Please write your code here.
+
+
+cnt,max_cnt = 0, 0
+
+for i in range(n) :
+    if i >= 0 and (arr[i] - arr[i-1] > 0) :
+        cnt += 1
+    else :
+        cnt = 1
+
+    max_cnt = max(max_cnt,cnt)
+
+
+print(max_cnt)
