b7d1c0a3026c4d9e746fb95439701757d7fd9ef0
naeunkim <83413364+naeunkim1227@users.noreply.github.com>
2025-06-27 12:35:30 +0900

    Title: 연속되는 수 2, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250627/연속되는 수 2/continuous-number2.py
+++ 250627/연속되는 수 2/continuous-number2.py
@@ -5,9 +5,17 @@ arr = [int(input()) for _ in range(n)]
 
 
 cnt = 0
+arr_cnt = 1
+blocks = []
 for i in range(n):
     if i == 0 or arr[i] != arr[i-1] :
         cnt += 1
+        blocks.append(arr_cnt)
+    else :
+        arr_cnt += 1
 
 
-print(cnt)
+    
+
+
+print(max(blocks))
