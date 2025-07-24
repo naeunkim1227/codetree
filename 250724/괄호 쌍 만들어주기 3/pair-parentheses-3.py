0dfcfd3e16de72a6c441d8534b6366012613f1a3
naeunkim <83413364+naeunkim1227@users.noreply.github.com>
2025-07-24 12:38:21 +0900

    Title: 괄호 쌍 만들어주기 3, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250724/괄호 쌍 만들어주기 3/pair-parentheses-3.py
@@ -0,0 +1,11 @@
+A = input()
+cnt = 0
+
+# Please write your code here.
+for i in range(len(A)) :
+    for j in range(i+1,len(A)) :
+        if A[i] == '(' and A[j] == ')' :
+            cnt+= 1
+
+
+print(cnt)
